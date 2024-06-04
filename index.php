4f12c0534531323d1010392d3d85b198cf561134
Nouiri Said <saidnouiri91@gmail.com>
2024-06-04 14:52:42 +0200

    test

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
 </head>
 <body>
 
-<h1>TEST 2</h1>
+<h1>TESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT</h1>
 <h2>Formulaire d'insertion</h2>
 
 <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
